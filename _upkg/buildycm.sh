01f0999180ad7b85e626269e6ede47bbfe130ad0
Jeff Buttars <jeffbuttars@gmail.com>
2013-12-19 13:38:09 -0700

    Fix ycm build script.

--- _upkg/buildycm.sh
+++ _upkg/buildycm.sh
@@ -6,7 +6,7 @@ build_ycm()
     if [[ -d ~/.vim/bundle/YouCompleteMe ]]; then
         pr_info "Building YouCompleteMe"
         cd ~/.vim/bundle/YouCompleteMe
-        cat $(git rev-parse HEAD) > $THIS_DIR/last_ycm
+        echo $(git rev-parse HEAD) > $THIS_DIR/last_ycm
         ./install.sh --clang-completer --system-libclang
 
         cd -
@@ -16,7 +16,7 @@ build_ycm()
 if [[ ! -f  $THIS_DIR/last_ycm ]]; then
     build_ycm
 else
-    cat $(git rev-parse HEAD) > $THIS_DIR/this_ycm
+    echo $(git rev-parse HEAD) > $THIS_DIR/this_ycm
     cmp $THIS_DIR/last_ycm  $THIS_DIR/this_ycm
     lc=$?
     if [[ "$lc" != "0" ]]; then
